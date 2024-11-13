db1b90deb39bd5f9306dfe808ed0e952d88c3770
webdron <webdron@gmail.com>
2024-11-13 21:48:21 +0300

    2 step Finished

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -5,6 +5,7 @@ def main():
     parser = argparse.ArgumentParser(prog='gendiff', description='Compares two configuration files and shows a difference.')
     parser.add_argument('first_file')
     parser.add_argument('second_file')
+    parser.add_argument('-f','--format', help='set format of output')
     args = parser.parse_args()
     print(args.first_file)
     print(args.second_file)
