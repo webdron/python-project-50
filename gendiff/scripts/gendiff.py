ee3812ec5078eedc30b82fde51b3a95ad8ea7478
webdron <webdron@gmail.com>
2024-11-19 21:11:34 +0300

    3 step Finished

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import argparse
+import json
 
 def main():
     parser = argparse.ArgumentParser(prog='gendiff', description='Compares two configuration files and shows a difference.')
@@ -7,8 +8,8 @@ def main():
     parser.add_argument('second_file')
     parser.add_argument('-f','--format', help='set format of output')
     args = parser.parse_args()
-    print(args.first_file)
-    print(args.second_file)
+    print(json.load(open(args.first_file)))
+    print(json.load(open(args.second_file)))
 
 if __name__ == '__main__':
     main()
