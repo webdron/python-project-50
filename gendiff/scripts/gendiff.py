86d1c6e56f4fee7be125cbc7b8ed0066b91b4e46
webdron <webdron@gmail.com>
2024-11-26 21:47:08 +0300

    4th step Finished

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -8,8 +8,30 @@ def main():
     parser.add_argument('second_file')
     parser.add_argument('-f','--format', help='set format of output')
     args = parser.parse_args()
-    print(json.load(open(args.first_file)))
-    print(json.load(open(args.second_file)))
+    generate_diff(args.first_file, args.second_file)
+
+def generate_diff(file1, file2):
+    file1 = json.load(open(file1))
+    file2 = json.load(open(file2))
+    allcase = set(file1.keys()).union(file2.keys())
+    res = []
+    for key in sorted(allcase):
+        value1 = json.dumps(file1.get(key))
+        value2 = json.dumps(file2.get(key))
+        if key in file1 and key in file2:
+            if value1 != value2:
+                res.append(f" - {key}: {value1}")
+                res.append(f" + {key}: {value2}")
+            else:
+                res.append(f"   {key}: {value1}")
+        elif key in file1:
+            res.append(f" - {key}: {value1}")
+        elif key in file2:
+            res.append(f" + {key}: {value2}")
+    result = "{\n" + "\n".join(res) + "\n}"
+    print(result)
+
+
 
 if __name__ == '__main__':
     main()
